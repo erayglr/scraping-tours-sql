5342781a9fe898a0bce76ab05e7c749ab83dd755
Eray <erayguler6757@gmail.com>
2024-04-03 13:37:49 +0300

    Addin datas sqlite database

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests
 import selectorlib
-import os
 import smtplib
 import ssl
 import time
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36'
                   ' (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+connection = sqlite3.connect('data.db')
 
 
 def scrape(url):
@@ -39,28 +40,35 @@ def send_email(message):
         server.login(username, password)
         server.sendmail(username, receiver, message)
 
+
 def store(extracted):
-    with open("data.txt", "a") as file:
-        if extracted not in read_data(file_local="data.txt"):
-            file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
-def read_data(file_local):
-    with open(file_local, "r") as file:
-        data = file.read()
-    return data
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band =? AND city =? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
 
 if __name__ == "__main__":
     while True:
-        if not os.path.exists("data.txt"):
-            with open("data.txt", "w") as file:
-                pass
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in read_data(file_local="data.txt"):
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event was found")
         time.sleep(2)
