9eaecbf4b94fa350aee84f1539842c180ca14e05
Eray <erayguler6757@gmail.com>
2024-04-04 13:43:15 +0300

    Adding OOP

--- main.py
+++ main.py
@@ -5,70 +5,76 @@ import ssl
 import time
 import sqlite3
 
-URL = "http://programmer100.pythonanywhere.com/tours/"
+URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36'
                   ' (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect('data.db')
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url)
-    source = response.text
-    return source
+class Email:
+    def send_email(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "erayguler5767@gmail.com"
+        password = "kzoi iekn ynsk ixli"
+
+        receiver = "erayguler5767@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Database:
 
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
-    username = "erayguler5767@gmail.com"
-    password = "kzoi iekn ynsk ixli"
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
 
-    receiver = "erayguler5767@gmail.com"
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
-
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band =? AND city =? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    return rows
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band =? AND city =? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="Hey, new event was found")
+                database.store(extracted)
+                email = Email()
+                email.send_email(message="Hey, new event was found")
         time.sleep(2)
